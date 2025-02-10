988a4524f8a0ebbd5f54c12953d8e69dc41d495b
Vorjarnon <chinchinfuguri@gmail.com>
2025-02-11 00:56:27 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -5,7 +5,12 @@ st.title("To Charmagne Gyle Z. Corpuz, Happy 3rd Anniversary!")
 
 # Love letter content
 love_letter = """
-Ito ay isang letra na nakatuon sa aking minamahal. Sa aking kasintahan, aking prinsesa, aking reyna, aking babie.
+Ito ay isang letra na nakatuon sa aking minamahal.
+
+aking kasintahan
+aking prinsesa
+aking reyna
+aking babie
 
 Nakalipas na ng 1096 na araw simula nung tayo ay naging opisyal. Dami nating napagdaanan, dami nating napagusapan, nalaman sa isa't isa, at marami pa. Ang mga taon na to ay napakasaya at hinding-hindi ko makakalimutan hanggang sa pagtanda natin. Ako ay nagpapasalamat na nakilala kita, at napakasaya at tanging maipagmamalaki na ikaw ang aking tanging babie. Ang taong 2024 ay napakasama saatin, noh? Tipong yung mga araw na nagkita tayo nung huling taon, pag pinagsama-sama ay parang isa o dalawang buwan lang. Nakakamiss yung mga panahon na kada linggo nagkikita tayo, minsan nga dumadating pa sa punto na araw-araw na. Nakakamiss yun. Sana itong taon na to ay mas maging isang taon na hinding-hindi natin makakalimutan. Sana yung mga pinaggagagawa natin noon, mabalik natin sa taon na to. Ikaw ay aking pinasasalamatan dahil ikaw ang nagbunyag saakin na karapat-dapat kong maramdaman ang pagmamahal, at ibigay din ito saiyo. Sa mga taon na to, ngayong tayo ay tatlong taon na, ako ay may malakas na loob na sabihin na ang nararamdaman ko sa iyo ay kailanman ay di nagbago at lalong-lalo na sa mga darating na panahon at taon, ay hinding-hindi lalo magbabago. Subalit, marami man akong dapat ipasalamat, marami rin akong dapat hingan ng patawad sa'yo.
 """
