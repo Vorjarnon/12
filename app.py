9bdd156916fbf7d03d75315beeecac05864f353c
Vorjarnon <chinchinfuguri@gmail.com>
2025-02-11 00:36:59 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -9,14 +9,13 @@ This is a letter dedicated to the woman I love. My significant other, my princes
 
 It's been 1096 days. It's one helluva ride. To be honest, I'll always say these words to you. The fact that I am very thankful to have met you, and the fact that I am very happy have you as my one and only babie. 2024 has been rough for the two of us, rough in a way that the days we met personally can be easily counted. I really, really miss the days that we can see each other in every week that passes. I hope this year will be the return of that, babie. I have you to thank for telling me that I deserve to feel, let alone express love. In these years, let me tell you, I'm confident.. VERY confident to say that what I feel for you hasn't changed. And it sure as hell will never change.
 
-I love you to the moon and back, always.
 """
 
 # Display the love letter
 st.write(love_letter)
 
 # Subheader for the image slideshow
-st.subheader("Pic Galleryyyy")
+st.subheader("I love you to the moon and back, always.")
 
 # List of image paths (update these paths to your actual images)
 image_paths = [
@@ -26,5 +25,5 @@ image_paths = [
 ]
 
 # Slider to select the current image
-image_index = st.slider("Select image", 0, len(image_paths) - 1)
+image_index = st.slider(0, len(image_paths) - 1)
 st.image(image_paths[image_index])
