34a854377eec6508b58e45cb5e36a5ea24e5bb27
Vorjarnon <chinchinfuguri@gmail.com>
2025-02-11 18:39:06 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -18,6 +18,9 @@ st.write(love_letter)
 st.header("I love you to the moon and back, always. 💖")
 st.title("Happy 3rd Anniversary!")
 
+# Add background music
+st.audio("msc/adoreyou.mp3")  # Update with your actual audio file path
+
 # List of image paths (update these paths to your actual images)
 image_paths = [
     "img/image1.jpg",
